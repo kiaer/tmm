4f0cfa64f9c756ef89983bce4c0c13039e330ea7
Anton <anton_almgren@hotmail.com>
2018-11-26 16:24:06 +0100

    cleaning up mit_tmm

--- src/MATLAB/mit_tmm.m
+++ src/MATLAB/mit_tmm.m
@@ -1,84 +1,85 @@
-% TMM
-%
-% Christian Ki??r and Anton Almgren
+%% TMM MIT 2.8 degrees GCM
+% This script is implemented to use the transport matrix method for
+% transport of passive tracers in a global ocean.
+% The method is described in detail here: https://github.com/samarkhatiwala/tmm
+% Transport matrices and related data are available at 
+% http://kelvin.earth.ox.ac.uk/spk/Research/TMM/TransportMatrixConfigs/
+% 
+% Authors: Christian Ki?r and Anton Almgren
+% Fall 2018
+
 clear all
 close all
 clc
 
-%%
-%load("../../bin/MITgcm/Matrix10/TMs/matrix_nocorrection.mat");
+%% Load transport matrices and configuration data
+
 load('../../bin/MITgcm/Matrix5/TMs/matrix_nocorrection_annualmean.mat');
 load('../../bin/MITgcm/grid.mat');
 load('../../bin/MITgcm/config_data.mat');
 load('../../bin/MITgcm/Matrix5/Data/boxes.mat')
 
-%%1
-surface(x,y,bathy(:,:,1)')
-shading flat
-C = zeros(128,64,15);
+%% Initializing arrays
 
-bathy(60,30,1)
-C(:,:,1) = 1;
+C = zeros(128,64,15);                   % Concentration matrix for tracer
+C(:,:,1) = 1;                           % Initial condition for tracer
 
-Ib=find(izBox>0);
-nbb=length(Ib);
-I = linspace(1,52749, 52749)';
-Ia = linspace(1, 128*64*15, 128*64*15)';
+% vector representation of C:
 mat = gridToMatrix(C, [], '../../bin/MITgcm/Matrix5/Data/boxes.mat', '../../bin/MITgcm/grid.mat');
-C = matrixToGrid(mat, [], '../../bin/MITgcm/Matrix5/Data/boxes.mat', '../../bin/MITgcm/grid.mat');
-figure
-contourf(C(:,:,1)', 5)
-colorbar
-caxis([0 10])
 
 
-%%
-Ix = speye(nb,nb);
-Aexpms = Ix + (12*60*60)*Aexpms;
-Aimpms = Aimpms^(36);
-%%
-H = 5371; %years
-D = (1/2)/(730*H); 
-Cn = mat;
+%% Preparing transport matrices for time stepping
 
-for i=1:730*10%365*6
+Ix = speye(nb,nb);                      % Sparse Identity matrix 
+Aexpms = Ix + (12*60*60)*Aexpms;        % Discretizing Aexpms for Explicit
+                                        % euler time stepping using 12h
+                                        % timesteps
+Aimpms = Aimpms^(36);                   % modifying Aimpms to use 12h timesteps
+%% C14 model
+H = 5000;                               % years
+D = (1/2)/(730*H);                      % decay
+Cn = mat;                               % initializing concentration
+
+% iterating through time for 10 yrs using 12h steps
+for i=1:730*10
+    % converting to grid format:
     Cn = matrixToGrid(Cn, [], '../../bin/MITgcm/Matrix5/Data/boxes.mat', '../../bin/MITgcm/grid.mat');
+    % setting surface to fixed value
     Cn(:,:,1) = 1;
+    % Converting concentration field to matrix format (vector)
     Cn = gridToMatrix(Cn, [], '../../bin/MITgcm/Matrix5/Data/boxes.mat', '../../bin/MITgcm/grid.mat');
+    % time stepping with decay
     Cn =  Aimpms * ( Aexpms  * Cn) - Cn * D;
-
 end
-%Cn = Cn ;
 %% Conservation of mass
-a = volb/sum(volb);
+% only consistent with closed boundary conditions and no source terms
+a = volb/sum(volb);         % relative volume
+% calculating in small bits for matlab's sake
 for i = 1:1000:52000
-    COM_c(i:i+1000) = a(i:i+1000).*mat(i:i+1000);
-    COM_cn(i:i+1000) = a(i:i+1000).*Cn(i:i+1000);
-
+    COM_c(i:i+1000) = a(i:i+1000).*mat(i:i+1000);   % mass in each cell initially
+    COM_cn(i:i+1000) = a(i:i+1000).*Cn(i:i+1000);   % mass in each cell after iteration
 end
 COM_c(52000:52749)=a(52000:52749).*mat(52000:52749);
 COM_cn(52000:52749)=a(52000:52749).*Cn(52000:52749);
 
-COM = sum(COM_c)-sum(COM_cn);
+COM = sum(COM_c)-sum(COM_cn);       % Conservation of mass. Should be small.
+%% Plotting
 
+% converting concentration vector to grid
 Cn = matrixToGrid(Cn, [], '../../bin/MITgcm/Matrix5/Data/boxes.mat', '../../bin/MITgcm/grid.mat');
 
-
-%%
-
+% contour plot of surface values
 figure
-contourf(Cn(55:65,20:40,1)', 3)
-colorbar
 contourf(Cn(:,:,1)', 5)
 colorbar
-%caxis([0 100])
+
+% Transect plot at longitude 30 (Atlantic)
 figure
 Cx = permute(Cn, [1,3,2]);
 contourf(x,z,Cx(:,:,30)',5)
-
 axis ij
 
-%%
+%% 
 figure
 Cy = permute(Cn, [2,3,1]);
 contourf(y(32:55),z,Cy(32:55,:,113)',6)
